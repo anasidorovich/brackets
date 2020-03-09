b0189de3e5b56c9593842680e5de5d205258007b
sidarova <anasidorovich@gmail.com>
2020-03-09 22:46:56 +0300

    feat: add solution for checking brackets sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        brackets.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+    }
+    let valid = true;
+    while (str.length > 1 && valid) {
+        let newStr = replaceBrackets(str, brackets);
+        if (str == newStr) {
+            valid = false;
+        } else {
+            str = newStr;
+        }
+    }
+    if (!valid || str.length == 1) {
+      return false;
+    }
+    return true;
 }
+
+function replaceBrackets(str, brackets) {
+    for (let i = 0; i < brackets.length; i++) {
+        if (str.includes(brackets[i])) {
+            str = str.replace(brackets[i], '');
+        }
+    }
+    return str;
+};
